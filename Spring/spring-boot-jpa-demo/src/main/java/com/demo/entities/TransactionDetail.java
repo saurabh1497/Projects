b6c3a28f6590b249abea97c54e65e5949e37b3e3
saurabh1497 <47416411+saurabh1497@users.noreply.github.com>
2020-11-12 16:27:11 +0530

    Add files via upload

---
+++ Spring/spring-boot-jpa-demo/src/main/java/com/demo/entities/TransactionDetail.java
@@ -0,0 +1,76 @@
+package com.demo.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class TransactionDetail {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long transactionId;
+	private Long accountNumber;
+	private Date transactionDate;
+	private int amount;	
+	
+	
+	private TransactionType txType;
+	
+	
+	
+	
+	public TransactionDetail( Long accountNumber,
+			Date transactionDate, int amount, TransactionType type) {
+		super();
+		
+		this.accountNumber = accountNumber;
+		this.transactionDate = transactionDate;
+		this.amount = amount;
+		this.txType = type;
+	}
+	
+	public TransactionDetail() {
+		
+	}
+
+	public Long getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(Long transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public Date getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public TransactionType getTxType() {
+		return txType;
+	}
+
+	public void setTxType(TransactionType txType) {
+		this.txType = txType;
+	}
+	
+	
+}
